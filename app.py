ced549fccb8aa0cab65449bd5ba6d4ad93d8a453
doit5168-collab <doit5168@gmail.com>
2026-05-19 20:31:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import datetime
+
 st.set_page_config(page_title="微型 TimeTree", layout="wide")
 with st.sidebar:
     st.write("###  行事曆群組")
@@ -18,6 +20,11 @@ with col_left:
     with st.container(border=True): 
         st.write(" 標題：開學典禮") 
         st.write(" 時間：09:00")
+        
+today = st.date_input(
+  "選擇日期",
+  datetime.date.today()
+)
         
 
 
