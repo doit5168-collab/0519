0e1c1c640bff89068ca655fcad582f67d5f6b3e4
doit5168-collab <doit5168@gmail.com>
2026-05-19 19:34:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with col_left:
 with col_center:
     write("###  看板區") st.info("主要行程訊息放中間")
     with st.expander("查看進階提醒參數設定"):
-        st.write("這裡是發信伺服器的底層設定...")"
+        st.write("這裡是發信伺服器的底層設定...")
 
 with col_right: st.
     write("###  設定區") st.button("控制項放右邊"
