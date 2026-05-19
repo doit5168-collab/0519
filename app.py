8c8c837faab9bd93216843678f521060c4eed104
doit5168-collab <doit5168@gmail.com>
2026-05-19 20:08:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ with col_right:
         st.write("本週作業請確認 requirements.txt 有正確設定！")
     if st.button("查看公告"): 
               show_alert()
-        st.write("上面是大標題")
-        st.divider()
-        st.write("下面是內容區塊")
+    st.write("上面是大標題")
+    st.divider()
+    st.write("下面是內容區塊")
 
     with st.popover("快速進階篩選"):
         st.checkbox("隱藏已過期行程")
