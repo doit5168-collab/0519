42c57a3a0d3aee1bff9ea376fe7fecce0664bd45
doit5168-collab <doit5168@gmail.com>
2026-05-19 19:23:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,23 @@ st.set_page_config(page_title="微型 TimeTree", layout="wide")
 with st.sidebar:
     st.write("###  行事曆群組")
     st.radio("選擇群組", ["工作", "家庭"])
+    
+col_left, col_center, col_right = st.columns([1, 2, 1.2], gap="large")
+
+with col_left: 
+    st.write("###  新增區") st.button("按鈕放左邊")
+    with st.container(border=True): 
+    st.write(" 標題：開學典禮") 
+    st.write(" 時間：09:00")
+
+with col_center:
+    write("###  看板區") st.info("主要行程訊息放中間")
+    with st.expander("查看進階提醒參數設定"):
+        st.write("這裡是發信伺服器的底層設定...")"
+
+with col_right: st.
+    write("###  設定區") st.button("控制項放右邊"
+                                @st.dialog("系統公告")
+def show_alert():
+    st.write("本週作業請確認 requirements.txt 有正確設定！")
+if st.button("查看公告"): show_alert()
